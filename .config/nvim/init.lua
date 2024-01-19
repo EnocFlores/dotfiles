6d24b715ed74e2fc88b6e931c87993a6cc5167b9
EnocFlores <33188190+EnocFlores@users.noreply.github.com>
2024-01-19 17:01:39 -0500

    Fixes Linux issue with making files named 1, small improvements to overall configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -672,5 +672,10 @@ cmp.setup {
   },
 }
 
+vim.keymap.set('i', '<C-J>', 'copilot#Accept(\"<CR>\")', {
+    expr = true,
+    replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = false
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
